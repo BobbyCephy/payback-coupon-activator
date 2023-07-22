2e7f4922f02e437aefabee47be9e36bead355a0f
Robert Kampf <Robert.Kampf@gmx.de>
2023-07-22 14:27:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Payback Coupon Activator
+Activates all Payback coupons with Selenium WebDriver in Chrome logged into your Payback account.
+
+## Installation
+* Install [Chrome](https://www.google.com/chrome)
+* Log in to your [Payback](https://www.payback.de/login) account permanently
+* Install this package: ```pip install payback-coupon-activator```
+
+## Usage
+* Close Chrome
+* Execute ```payback-coupon-activator```
+
+## Support
+Create an [issue](https://github.com/BobbyCephy/payback-coupon-activator/issues) for problems, improvements, different [browsers](https://github.com/SeleniumHQ/selenium/tree/trunk/py/selenium/webdriver) or [operating systems](https://chromium.googlesource.com/chromium/src/+/master/docs/user_data_dir.md)
---
+++ payback_coupon_activator.py
@@ -0,0 +1,47 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+import os
+
+
+def element(by, value, parent, timeout=10):
+    return WebDriverWait(parent, timeout).until(
+        expected_conditions.presence_of_element_located((by, value))
+    )
+
+
+def main():
+    options = Options()
+    options.add_argument(
+        "user-data-dir="
+        + os.path.join(
+            os.getenv("LOCALAPPDATA"), "Google", "Chrome", "User Data"
+        )
+    )
+    service = Service()
+    driver = Chrome(options, service)
+    driver.get("https://www.payback.de/coupons")
+    center = element(By.ID, "coupon-center", driver).shadow_root
+    headline = element(
+        By.CLASS_NAME, "coupon-center__header-published-headline", center
+    )
+    container = element(
+        By.CLASS_NAME, "coupon-center__container-published-coupons", center
+    )
+    coupons = container.find_elements(By.TAG_NAME, "pbc-coupon")
+
+    for index, coupon in enumerate(coupons):
+        button = element(
+            By.CSS_SELECTOR, "pbc-coupon-call-to-action", coupon.shadow_root
+        )
+        button.click()
+        print("Coupon", index + 1, "/", len(coupons), "activated")
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
